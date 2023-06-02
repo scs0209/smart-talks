e1906cef822f28d5bf77316151a46779b7e0f868
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-06-02 20:08:53 +0900

    open ai 무료 평가판 기간 지남으로 인한 서비스 중단

--- src/pages/api/ai/qna.ts
+++ src/pages/api/ai/qna.ts
@@ -19,6 +19,8 @@ export default async function handler(
     return
   }
 
+  console.log('!configuration.apiKey: ', !configuration.apiKey)
+
   const question: string = req.body.question || ''
 
   const request: CreateCompletionRequest = {
