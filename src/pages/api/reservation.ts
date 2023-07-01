41c14d5f7d6d796882ec64d99d1849288704a65f
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-07-01 18:02:19 +0900

    feat: 영화 예매 api 생성

--- src/pages/api/reservation.ts
+++ src/pages/api/reservation.ts
@@ -2,31 +2,62 @@ import Reservation, { IReservation } from '@/models/Reservation'
 import connectDB from '@/services/dbConnect'
 import { NextApiRequest, NextApiResponse } from 'next'
 
+const saveReservation = async (reservationData: IReservation) => {
+  try {
+    const reservation = new Reservation(reservationData)
+    await reservation.save()
+    return reservation
+  } catch (error) {
+    console.error('Error saving reservation data:', error)
+    return null
+  }
+}
+
+const getReservationsByUserId = async (user_id: string) => {
+  try {
+    const reservations = await Reservation.find({ user_id }).populate(
+      'showtime_id',
+    )
+    return reservations
+  } catch (error) {
+    console.error('Error fetching reservations:', error)
+    return null
+  }
+}
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   await connectDB()
 
-  if (req.method === 'GET') {
-    const reservations = await Reservation.find({})
-    res.status(200).json({ reservations })
-  } else if (req.method === 'POST') {
+  if (req.method === 'POST') {
+    const reservationData = req.body as IReservation
+
     try {
-      const newReservation: IReservation = new Reservation({
-        // 필요한 필드를 모두 추가하세요.
-      })
-      await newReservation.save()
-      res.status(200).json({
-        success: true,
-        reservation: newReservation,
-      })
-    } catch (error: unknown) {
-      const message =
-        error instanceof Error ? error.message : 'Unknown error occurred'
-      res.status(400).json({ success: false, message: message })
+      const savedReservation = await saveReservation(reservationData)
+      if (savedReservation) {
+        res.status(200).json(savedReservation)
+      } else {
+        res.status(500).json({ error: 'Error saving reservation data' })
+      }
+    } catch (error) {
+      res.status(500).json({ error: 'Error saving reservation data' })
+    }
+  } else if (req.method === 'GET') {
+    const { user_id } = req.query
+
+    if (typeof user_id === 'string') {
+      const reservations = await getReservationsByUserId(user_id)
+      if (reservations) {
+        res.status(200).json(reservations)
+      } else {
+        res.status(500).json({ error: 'Error fetching reservations' })
+      }
+    } else {
+      res.status(400).json({ error: 'Invalid user_id' })
     }
   } else {
-    res.status(405).json({ message: 'Unsupported method.' })
+    res.status(405).json({ error: 'Method not allowed' })
   }
 }
