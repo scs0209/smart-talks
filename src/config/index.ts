1bd5b342d687a92516c2798c81ee602b79bbf3d7
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-08-09 15:49:10 +0900

    chore: api 요청 주소 변경

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,1 +1,4 @@
-export const backUrl = 'http://localhost:3000'
+export const backUrl =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'https://smart-talks.vercel.app/'
