85b2912b0a85b62d679843755e278225dbc3a17f
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-11-14 20:49:43 +0900

    fix: 3D text에 색깔 안나오는 에러 해결

--- src/components/Home/Movix.tsx
+++ src/components/Home/Movix.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Canvas } from '@react-three/fiber'
+import React, { useMemo, useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
 import {
   Text3D,
   OrbitControls,
@@ -27,27 +27,44 @@ const GradientShaderMaterial = shaderMaterial(
   `,
 )
 
+const TextComponent = () => {
+  const gradientShaderMaterial = useMemo(() => new GradientShaderMaterial(), [])
+  const textRef = useRef<any>()
+
+  useFrame(() => {
+    if (textRef.current) {
+      textRef.current.material = gradientShaderMaterial
+      textRef.current.material.needsUpdate = true
+    }
+  })
+
+  return (
+    <Center rotation={[-0.2, -0.25, 0]}>
+      <Text3D
+        ref={textRef}
+        font="/Inter_Bold.json"
+        curveSegments={32}
+        bevelSize={0.04}
+        bevelThickness={0.1}
+        height={0.5}
+        lineHeight={0.5}
+        letterSpacing={-0.06}
+        size={1.5}
+        material={gradientShaderMaterial}
+      >
+        FILM FINDER
+        <meshBasicMaterial />
+      </Text3D>
+    </Center>
+  )
+}
+
 const Movix = () => {
   return (
     <Canvas>
       <ambientLight intensity={0.1} />
       <pointLight position={[5, 5, 5]} />
-      <Center rotation={[-0.2, -0.25, 0]}>
-        <Text3D
-          font="/Inter_Bold.json"
-          curveSegments={32}
-          bevelSize={0.04}
-          bevelThickness={0.1}
-          height={0.5}
-          lineHeight={0.5}
-          letterSpacing={-0.06}
-          size={1.5}
-          material={new GradientShaderMaterial()}
-        >
-          FILM FINDER
-          <meshBasicMaterial />
-        </Text3D>
-      </Center>
+      <TextComponent />
       <OrbitControls />
     </Canvas>
   )
