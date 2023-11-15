d5259d874bc6fbf3c15bac1a9212e619acb2fb3c
scs0209 <110822847+scs0209@users.noreply.github.com>
2023-11-15 15:16:05 +0900

    feat: react-toastify 초기 셋팅

---
+++ src/constants/toastMessage.ts
@@ -0,0 +1,15 @@
+export const TOAST_MESSAGE = {
+  // auth
+  LOGIN_SUCCESS: '로그인에 성공했습니다.',
+  SIGNUP_SUCCESS: '회원가입에 완료되었습니다.',
+  LOGOUT_SUCCESS: '로그아웃되었습니다.',
+  DELETE_USER_SUCCESS: '계정이 삭제되었습니다.',
+
+  // password
+  FIND_PASSWORD_SUCCESS: '임시 비밀번호 발급 완료',
+  UPDATE_PASSWORD_SUCCESS: '비밀번호 변경 완료',
+  ADD_SUCCESS: '성공적으로 추가되었습니다.',
+  ADD_FAIL: '추가하는데 실패했습니다.',
+  DELETE_SUCCESS: '성공적으로 삭제되었습니다.',
+  DELETE_FAIL: '삭제하는데 실패했습니다.',
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,8 @@ import { SessionProvider } from 'next-auth/react'
 import Layout from '@/components/Layout'
 import { wrapper } from '@/redux/store'
 import ProtectedLayout from '@/components/ProtecetedLayout'
+import { ToastContainer, Zoom } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 type AppPropsWithAuth = AppProps & {
   Component: {
@@ -19,6 +21,13 @@ function App({ Component, pageProps }: AppPropsWithAuth) {
   return (
     <SessionProvider session={...pageProps.session}>
       <Flowbite>
+        <ToastContainer
+          position="top-center"
+          autoClose={1000}
+          hideProgressBar
+          transition={Zoom}
+          limit={1}
+        />
         {Component.requireAuth ? (
           <ProtectedLayout allowRole={Component.allowRole}>
             <Component {...pageProps} />
